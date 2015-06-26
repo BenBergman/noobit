3a686ef6b43135e583e69d38a45120241bbc1744
Benjamin Bergman <ben@benbergman.ca>
2015-06-25 19:30:13 -0500

    Add markov chain handler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #[macro_use(handler)]
 extern crate chatbot;
+extern crate markov;
+
+use std::path::Path;
 
 use chatbot::Chatbot;
 use chatbot::adapter::{IrcAdapter, IrcConfig};
 use chatbot::handler::GithubIssueLinker;
+
+use markov::Chain;
 
 fn main() {
     let name = "noobit";
@@ -13,6 +18,13 @@ fn main() {
         Some(msg.to_owned())
     });
     let ping = handler!("PingHandler", r"ping", |_, _| Some("pong".to_owned()));
+
+    let mut chain = Chain::for_strings();
+    chain.feed_file(Path::new("sksp"));
+
+    let markov = handler!("Markov", r"markov", move |_, _| {
+        Some(chain.str_iter().next().unwrap())
+    });
 
     let irc = IrcAdapter::new(IrcConfig {
         nickname: Some(format!("{}", name)),
@@ -25,6 +37,8 @@ fn main() {
     bot.add_handler(echo);
     bot.add_addressed_handler(ping);
     bot.add_handler(GithubIssueLinker::new());
+    bot.add_handler(markov);
+
     bot.add_adapter(irc);
 
     bot.run();
